5510bd189d3aa002bc933cea2ee946e037e26555
hasnain <m.hasnynmdarain@gmail.com>
2023-10-15 22:16:00 +0200

    updated controller

--- src/main/java/com/aws/codestar/projecttemplates/controller/RailwayController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/RailwayController.java
@@ -3,7 +3,6 @@ package com.aws.codestar.projecttemplates.controller;
 import com.aws.codestar.projecttemplates.model.CheckConflictRequest;
 import com.aws.codestar.projecttemplates.model.CheckConflictResponse;
 import com.aws.codestar.projecttemplates.service.PathFinderServiceImpl;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +21,8 @@ public class RailwayController {
         return ResponseEntity.ok("Hello, the service is up!");
     }
 
-    @RequestMapping(path = "/check_conflicts", method = RequestMethod.POST, produces = "application/json")
+    //@RequestMapping(path = "/check_conflicts", method = RequestMethod.POST, produces = "application/json")
+    @PostMapping("/check_conflicts")
     public ResponseEntity<CheckConflictResponse> checkConflicts(@RequestBody CheckConflictRequest request) {
         boolean isPathValid = pathFinderServiceImpl.checkConflicts(request);
         return ResponseEntity.ok(new CheckConflictResponse(isPathValid));
