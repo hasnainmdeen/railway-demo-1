c2dedce9575de3271c90c5ef16aca783873daed5
hasnain <m.hasnynmdarain@gmail.com>
2023-10-15 16:43:39 +0200

    cloned code star project to local

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -2,10 +2,7 @@ package com.aws.codestar.projecttemplates.controller;
 
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Basic Spring web service controller that handles all GET requests.
@@ -17,11 +14,12 @@ public class HelloWorldController {
     private static final String MESSAGE_FORMAT = "Hello %s!";
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World - so the annotation thingy works!") String name) {
         return ResponseEntity.ok(createResponse(name));
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @PostMapping("/test")
+    //@RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "World") String name) {
         return ResponseEntity.ok(createResponse(name));
     }
