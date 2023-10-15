af1553ebdcb251f776f88e5658c0a5cbe68e0108
hasnain <m.hasnynmdarain@gmail.com>
2023-10-15 20:32:09 +0200

    updated controller

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import com.aws.codestar.projecttemplates.model.CheckConflictResponse;
+import com.aws.codestar.projecttemplates.service.PathFinderServiceImpl;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloWorldController {
 
+
+    @Autowired
+    private PathFinderServiceImpl pathFinderServiceImpl;
     private static final String MESSAGE_FORMAT = "Hello %s!";
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
@@ -22,8 +28,10 @@ public class HelloWorldController {
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return ResponseEntity.ok(createResponse(name));
+    public ResponseEntity<CheckConflictResponse> helloWorldPost(@RequestParam(value = "name", defaultValue = "World") String name) {
+        //return ResponseEntity.ok(createResponse(name));
+        boolean isPathValid = pathFinderServiceImpl.checkConflicts(request);
+        return ResponseEntity.ok(new CheckConflictResponse(isPathValid));
     }
 
     private String createResponse(String name) {
