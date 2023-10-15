2fca4a0a3522a6d32fc20e84351be3d524f01529
hasnain <m.hasnynmdarain@gmail.com>
2023-10-15 17:51:36 +0200

    implemented endpoint in controller

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -2,7 +2,9 @@ package com.aws.codestar.projecttemplates.controller;
 
 import com.aws.codestar.projecttemplates.model.CheckConflictRequest;
 import com.aws.codestar.projecttemplates.model.CheckConflictResponse;
+import com.aws.codestar.projecttemplates.service.PathFinderServiceImpl;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,24 +12,15 @@ import org.springframework.web.bind.annotation.*;
  * Basic Spring web service controller that handles all GET requests.
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/railway")
 public class HelloWorldController {
 
-    private static final String MESSAGE_FORMAT = "Hello %s!";
+    @Autowired
+    private PathFinderServiceImpl pathFinderServiceImpl;
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World - so the annotation thingy works!") String name) {
-        return ResponseEntity.ok(createResponse(name));
-    }
-
-    //@PostMapping("/test")
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @PostMapping("/check_conflicts")
     public ResponseEntity<CheckConflictResponse> helloWorldPost(@RequestBody CheckConflictRequest request) {
-        return ResponseEntity.ok(new CheckConflictResponse(true));
-        //return ResponseEntity.ok(createResponse(name));
-    }
-
-    private String createResponse(String name) {
-        return new JSONObject().put("Output", String.format(MESSAGE_FORMAT, name)).toString();
+        boolean isPathValid = pathFinderServiceImpl.checkConflicts(request);
+        return ResponseEntity.ok(new CheckConflictResponse(isPathValid));
     }
 }
