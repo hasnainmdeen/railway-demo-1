901417d4406b777636e040f3a0632e4995379f97
hasnain <m.hasnynmdarain@gmail.com>
2023-10-15 22:47:14 +0200

    updated controller

--- src/main/java/com/aws/codestar/projecttemplates/controller/RailwayController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/RailwayController.java
@@ -4,6 +4,7 @@ import com.aws.codestar.projecttemplates.model.CheckConflictRequest;
 import com.aws.codestar.projecttemplates.model.CheckConflictResponse;
 import com.aws.codestar.projecttemplates.service.PathFinderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,9 +24,15 @@ public class RailwayController {
 
     //@RequestMapping(path = "/check_conflicts", method = RequestMethod.POST, produces = "application/json")
     @PostMapping("/check_conflicts")
-    public ResponseEntity<CheckConflictResponse> checkConflicts(@RequestBody CheckConflictRequest request) {
-        boolean isPathValid = pathFinderServiceImpl.checkConflicts(request);
-        return ResponseEntity.ok(new CheckConflictResponse(isPathValid));
+    public ResponseEntity<?> checkConflicts(@RequestBody CheckConflictRequest request) {
+        try {
+            boolean isPathValid = pathFinderServiceImpl.checkConflicts(request);
+            return ResponseEntity.ok(new CheckConflictResponse(isPathValid));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+
     }
 
 }
