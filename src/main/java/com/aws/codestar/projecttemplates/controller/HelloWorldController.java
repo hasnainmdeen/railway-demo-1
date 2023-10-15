3d7620f7e678cd30e117088a41746c21f7e0acd9
hasnain <m.hasnynmdarain@gmail.com>
2023-10-15 19:46:12 +0200

    updated controller

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,10 +1,11 @@
 package com.aws.codestar.projecttemplates.controller;
 
-import com.aws.codestar.projecttemplates.model.CheckConflictRequest;
-import com.aws.codestar.projecttemplates.model.CheckConflictResponse;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Basic Spring web service controller that handles all GET requests.
@@ -16,15 +17,13 @@ public class HelloWorldController {
     private static final String MESSAGE_FORMAT = "Hello %s!";
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World - so the annotation thingy works!") String name) {
+    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World") String name) {
         return ResponseEntity.ok(createResponse(name));
     }
 
-    //@PostMapping("/test")
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<CheckConflictResponse> helloWorldPost(@RequestBody CheckConflictRequest request) {
-        return ResponseEntity.ok(new CheckConflictResponse(true));
-        //return ResponseEntity.ok(createResponse(name));
+    public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return ResponseEntity.ok(createResponse(name));
     }
 
     private String createResponse(String name) {
